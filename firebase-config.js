f2caa0a2bfc797c706cc455702311dad5ac45a95
chest050 <b6040620@gmail.com>
2025-04-14 13:42:58 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-app",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCTcY6_sNlZIgHYJUx9-L5xvEZKRtasHVM",
+  authDomain: "mychat-977be.firebaseapp.com",
+  projectId: "mychat-977be",
+  storageBucket: "mychat-977be.firebasestorage.app",
+  messagingSenderId: "520205943601",
+  appId: "1:520205943601:web:416a33aae16ff98bb8e610",
+  measurementId: "G-BG439KPR5M"
 };
- 
